5a66ec9775ecbaa008cbf6fca729b992f64c3fd2
paytonZ^.- <juanlp01@estumail.ucm.es>
2013-05-06 13:37:45 +0200

    Idea modelo

---
+++ Practica-IS/src/app/bsodsoftware/gameclub/java/modelo/SistemaJuegos.java
@@ -0,0 +1,32 @@
+package app.bsodsoftware.gameclub.java.modelo;
+
+import app.bsodsoftware.gameclub.java.entidades.juego.Juego;
+import app.bsodsoftware.gameclub.java.modelo.fachadas.InterfazFachadaJuego;
+
+public class SistemaJuegos implements InterfazFachadaJuego {
+
+	@Override
+	public Juego addjuego(Juego juego) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Juego modificarjuego(Juego juego) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean borrarjuego(Integer idJuego) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean existejuego(Integer idJuego) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ Practica-IS/src/app/bsodsoftware/gameclub/java/modelo/SistemaPrestamos.java
@@ -0,0 +1,33 @@
+package app.bsodsoftware.gameclub.java.modelo;
+
+import app.bsodsoftware.gameclub.java.entidades.prestar.Prestamo;
+import app.bsodsoftware.gameclub.java.entidades.usuarios.Usuario;
+import app.bsodsoftware.gameclub.java.modelo.fachadas.InterfazFachadaPrestamo;
+
+public class SistemaPrestamos implements InterfazFachadaPrestamo {
+
+	@Override
+	public Usuario addPrestamo(Prestamo prestamo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Usuario modificarPrestamo(Prestamo prestamo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean borrarPrestamo(Integer idPrestamo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean existePrestamo(Integer idPrestamo) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
