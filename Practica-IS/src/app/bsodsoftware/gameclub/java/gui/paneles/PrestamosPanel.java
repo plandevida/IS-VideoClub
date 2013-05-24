7e0abed703220c647a90f6a7cc627630511e7ba0
plandevida <plandevida@planes.com>
2013-05-24 18:02:34 +0200

    Sección de prestamos ahora en pestañas. Nuevo panel para ver los prestamos.

---
+++ Practica-IS/src/app/bsodsoftware/gameclub/java/gui/paneles/PrestamosPanel.java
@@ -0,0 +1,39 @@
+package app.bsodsoftware.gameclub.java.gui.paneles;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import app.bsodsoftware.gameclub.java.gui.ventanas.VentanaPrincipal;
+import app.bsodsoftware.gameclub.java.modelo.Sistema;
+
+public class PrestamosPanel extends JTabbedPane {
+
+	private static final long serialVersionUID = 1L;
+
+	private Sistema miSistema;
+	private VentanaPrincipal miVentana;
+	
+	public PrestamosPanel(Sistema sistema, VentanaPrincipal ventana) {
+		
+		miSistema = sistema;
+		miVentana = ventana;
+		
+		init();
+	}
+	
+	private void init() {
+		
+		addTab("Prestamo de juegos", crearPrestamosJuegosPanel());
+		addTab("Prestamos", crearPrestamoInfoPanel());
+	}
+	
+	private JPanel crearPrestamosJuegosPanel() {
+		
+		return new PrestamoJuegosPanel(miSistema, miVentana);
+	}
+	
+	private JPanel crearPrestamoInfoPanel() {
+		
+		return new JPanel();
+	}
+}
